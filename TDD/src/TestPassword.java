daac8a01a2574ea51a64278627e1f81f8bfdedc6
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-09 20:26:42 +0530

    first commit

---
+++ TDD/src/TestPassword.java
@@ -0,0 +1,10 @@
+import com.sun.tools.javac.util.Assert;
+
+public class TestPassword {
+	@test
+	public void testPassword() {
+		PasswordValidation pv = new PasswordValidation() ; 
+		Assert.a
+	}
+
+}
